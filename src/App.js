de3b3fc00ecd2570447143844eb7a57bd441b320
Дедюрин Виталий Николаевич <DedyurinVN@moscow.gbumac.ru>
2022-01-17 17:10:09 +0300

    add skelet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
 function App() {
   return (
-    <div>
-      Blog
+    <div className="main">
+      <div className="main__menu menu">
+        <div className="menu__btn">
+          <div className="menu__btn-text">Menu</div>
+        </div>
+      </div>
+      <div className="main__left">
+        letf
+        <div>Obout</div>
+      </div>
+      <div className="main__right">
+        rigth
+        <div>Header</div>
+        <div>ItemList</div>
+      </div>
     </div>
   );
 }
